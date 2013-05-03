825aab84605831e3d3aaf7d8d8b0ebf46fbfe8b0
Konstantin Ryabitsev <mricon@kernel.org>
2013-05-03 09:26:21 -0400

    Make sure leading dirs are in place for symlinks

--- grok-pull.py
+++ grok-pull.py
@@ -430,6 +430,8 @@ def pull_mirror(name, config, opts):
                 target = os.path.join(config['toplevel'], symlink.lstrip('/'))
                 if not os.path.exists(target):
                     logger.info('Symlinking %s -> %s' % (target, source))
+                    # Make sure the leading dirs are in place
+                    os.makedirs(os.path.dirname(target))
                     os.symlink(source, target)
 
     if opts.purge:
